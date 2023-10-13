a61ab36b3831f75b327d63db892d428df6cb116a
Daniel235-web <samueledaniel235@gmail.com>
2023-10-13 17:14:44 +0100

    created update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -6,7 +6,7 @@ export const createListing = async (req, res, next) => {
         const listing = await Listing.create(req.body);
         return res.status(201).json(listing);
     } catch (error) {
-        next();
+        next(error);
     }
 }
 
@@ -22,10 +22,31 @@ export const deleteListing = async (req, res, next) => {
 
     try {
         await Listing.findByIdAndDelete(req.params.id);
-        res.status(200).json ('Listing has been deleted!');
+        res.status(200).json({ message: 'Listing has been deleted!' });
+  
 
     } catch (error) {
         next(error);
     }
  
+};
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+    }
+    if (req.user.id !== listing.userRef) {
+        return next(errorHandler(401, 'You can only update your own listing!'));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error);
+    }
 };
