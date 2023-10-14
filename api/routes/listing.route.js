95dee7d6ee1e9a2c5df2b2c464dcdea87884e809
Daniel235-web <samueledaniel235@gmail.com>
2023-10-14 02:27:11 +0100

    completed update listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -12,5 +12,5 @@ router.delete('/delete/:id', verifyToken, deleteListing);
 
 // Use a colon before "id" to define it as a route parameter
 router.post('/update/:id', verifyToken, updateListing);
-
+router.get('/get/:id',  getListing);
 export default router;
