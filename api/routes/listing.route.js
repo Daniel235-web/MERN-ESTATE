d6217910b950b9508dedd60965212a43dc65334c
Daniel235-web <samueledaniel235@gmail.com>
2023-10-15 20:33:21 +0100

    Created Search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,16 +1,14 @@
 import express from 'express';
-import { createListing, deleteListing, updateListing, getListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing, getListing, getListings } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-// Use proper route parameter definition with a colon (:id)
+
 router.post('/create', verifyToken, createListing);
-
-// Use a colon before "id" to define it as a route parameter
 router.delete('/delete/:id', verifyToken, deleteListing);
-
-// Use a colon before "id" to define it as a route parameter
 router.post('/update/:id', verifyToken, updateListing);
 router.get('/get/:id',  getListing);
+router.get('/get', getListings);
+
 export default router;
