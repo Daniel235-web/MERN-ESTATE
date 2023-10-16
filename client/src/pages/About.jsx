9feb6f8308635e1ee0253eb5587cc24312c0d4b8
Daniel235-web <samueledaniel235@gmail.com>
2023-10-16 01:23:47 +0100

    completed the about page.. whooh!.. finished project!

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About SED Estate</h1>
+      <p className='mb-4 text-slate-700'>SED Estate stands as a preeminent real estate agency, specializing in facilitating clients in their pursuit of property acquisition, sales, and leasing within highly coveted neighborhoods. Our accomplished team of seasoned agents is wholeheartedly committed to delivering superlative service and ensuring a seamless and hassle-free experience throughout the buying and selling journey..</p>
+      <p className='mb-4 text-slate-700'>
+      Our overarching mission is to empower our valued clients to realize their real estate aspirations. We accomplish this by offering expert counsel, personalized guidance, and an in-depth comprehension of the local real estate landscape. Regardless of whether you seek to purchase, vend, or lease a property, our steadfast commitment is to stand by your side, offering unwavering support at every juncture of your real estate journey.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of seasoned agents boasts a rich reservoir of experience and an extensive knowledge base within the intricate domain of the real estate industry. We maintain an unwavering commitment to deliver the utmost level of service excellence to our clientele. We firmly uphold the conviction that the process of property acquisition or sale should be an exhilarating and gratifying journey, and we are wholly dedicated to actualizing this vision for each and every one of our esteemed clients.</p>
+    </div>
   )
 }
